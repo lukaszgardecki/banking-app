ed3908711459f0ccedbb64ced519dfdf7e32886c
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-03-27 20:18:38 +0200

    Change start class name

--- src/main/java/com/example/app/BankApp.java
+++ src/main/java/com/example/app/BankApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Website3Application {
+public class BankApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(Website3Application.class, args);
+        SpringApplication.run(BankApp.class, args);
     }
 
 }
