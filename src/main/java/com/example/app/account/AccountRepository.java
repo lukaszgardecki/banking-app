c0161b9156eaab32505ad0809a16a2afff205992
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-03-31 19:58:39 +0200

    Create account number generator

--- src/main/java/com/example/app/account/AccountRepository.java
+++ src/main/java/com/example/app/account/AccountRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 public interface AccountRepository extends CrudRepository<Account, Long> {
 
@@ -13,4 +14,7 @@ public interface AccountRepository extends CrudRepository<Account, Long> {
 
     @Query(value = "SELECT balance FROM accounts WHERE user_id = :id", nativeQuery = true)
     BigDecimal getTotalBalance(Long id);
+
+    @Query(value = "SELECT id FROM accounts WHERE id = (SELECT MAX(id) FROM accounts)", nativeQuery = true)
+    Optional<BigDecimal> getMaxId();
 }
--- src/main/java/com/example/app/account/AccountService.java
+++ src/main/java/com/example/app/account/AccountService.java
@@ -7,6 +7,9 @@ import java.util.List;
 
 @Service
 public class AccountService {
+    public static final int BANK_NUM = 12772055;
+    public static final int USER_NUM_LEN = 16;
+    public static final int CONTROL_SUM_LEN = 2;
     private final AccountRepository accountRepository;
 
     public AccountService(AccountRepository accountRepository) {
@@ -20,5 +23,40 @@ public class AccountService {
 
     public BigDecimal getTotalBalance(Long user_id) {
         return accountRepository.getTotalBalance(user_id);
+    }
+
+    public String generateAccountNumber() {
+        BigDecimal newId = accountRepository.getMaxId()
+                                .map(id -> id.add(BigDecimal.ONE))
+                                .orElse(BigDecimal.ONE);
+        String userNumber = createUserNum(newId);
+        String controlSum = getControlSum(userNumber);
+        return controlSum + BANK_NUM + userNumber;
+    }
+
+    private String getControlSum(String userNum) {
+        String preAccNum = BANK_NUM + userNum;
+        while (preAccNum.length() != CONTROL_SUM_LEN) {
+            preAccNum = doSumLoop(preAccNum);
+        }
+        return preAccNum;
+    }
+
+    private String doSumLoop(String num) {
+        int increasingParam = 3;
+        int sum = 0;
+
+        for (int i = 0; i < num.length(); i++) {
+            String digitStr = Character.toString(num.charAt(i));
+            int digitInt = Integer.parseInt(digitStr);
+            sum += (digitInt * increasingParam);
+        }
+        return String.valueOf(sum);
+    }
+
+    private String createUserNum(BigDecimal maxId) {
+        int amountOfZeros = USER_NUM_LEN - maxId.toString().length();
+        String leadZeros = "0".repeat(amountOfZeros);
+        return leadZeros + maxId;
     }
 }
