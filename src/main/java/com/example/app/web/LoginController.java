0cd16c88155b51133c0af80ba0407ddcdb0aafa8
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-04-01 22:03:50 +0200

    Change account verifying
    If user is not verified, he won't log in.
    Create CustomLogoutSuccessHandler to distinguish
    the logging out reasons.
    There are two cases of logging out:
       - normal (/login?logout)
       - when account is not verified (/login?verify)
    Different params allow displaying different messages
    in the login.html

--- src/main/java/com/example/app/web/LoginController.java
+++ src/main/java/com/example/app/web/LoginController.java
@@ -1,18 +1,9 @@
 package com.example.app.web;
 
-import com.example.app.user.User;
-import com.example.app.user.UserRepository;
 import com.example.app.user.UserService;
-import com.example.app.user.dto.UserDashboardDto;
-import jakarta.servlet.http.HttpSession;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.Optional;
 
 @Controller
 public class LoginController {
@@ -27,16 +18,11 @@ public class LoginController {
         return "login";
     }
 
-    @PostMapping("/login")
-    public String process(@RequestParam("username") String username, Model model, HttpSession session) {
-        boolean userVerified = userService.isUserVerified(username);
-        if (userVerified) {
-            userService.findUserByEmail(username)
-                    .ifPresent(user -> session.setAttribute("user", user));
-            return "redirect:/app/dashboard";
-        }
-        model.addAttribute("errorMsg", "This account is not verified yet. Please check email and verify your account.");
-        return "login";
+    @GetMapping("/login-verify")
+    public String checkVerification() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        boolean userIsNotVerified = !userService.isUserVerified(username);
+        if (userIsNotVerified) return "redirect:/logout?verified=false";
+        return "redirect:/app/dashboard";
     }
-
 }
