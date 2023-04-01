76a99a4987430e5dab1ceda266dacee31c50c7cf
Łukasz <105553857+lukaszgardecki@users.noreply.github.com>
2023-04-02 00:45:45 +0200

    Set bank account display (#12)
    * Add necessary fields to dashboard dto
    
    * Add Thymeleaf to account list

--- src/main/java/com/example/app/account/mappers/AccountDashboardMapper.java
+++ src/main/java/com/example/app/account/mappers/AccountDashboardMapper.java
@@ -7,10 +7,12 @@ public class AccountDashboardMapper {
 
     public static AccountDashboardDto map(Account account) {
         AccountDashboardDto dto = new AccountDashboardDto();
+        dto.setId(account.getId());
         dto.setAccountNumber(account.getAccount_number());
         dto.setAccountName(account.getAccount_name());
         dto.setAccountType(account.getAccount_type());
         dto.setBalance(account.getBalance());
+        dto.setCreatedAt(account.getCreated_at());
         return dto;
     }
 }
