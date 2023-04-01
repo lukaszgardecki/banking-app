1fda84fad38470910acd14cb35b8eb6a2412d76a
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-04-02 00:43:51 +0200

    Add necessary fields to dashboard dto

--- src/main/java/com/example/app/account/dto/AccountDashboardDto.java
+++ src/main/java/com/example/app/account/dto/AccountDashboardDto.java
@@ -4,12 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 public class AccountDashboardDto {
+    private Long id;
     private String accountNumber;
     private String accountName;
     private String accountType;
     private BigDecimal balance;
+    private LocalDateTime createdAt;
 }
