e4e5d51a180ce43ddf2e547a0bc562bdc138161b
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-04-02 13:45:20 +0200

    Create test data (accounts)

---
+++ src/main/resources/db/testdata/0003_accounts.sql
@@ -0,0 +1,7 @@
+insert into accounts
+    (user_id, account_number, account_name, account_type, balance, created_at, updated_at)
+values
+    (4, '90127720550000000000000001', 'Konto Główne', 'check', 450352.20, '2023-03-15 13:34:48.713868', null),
+    (4, '90127720550000000000000002', 'Konto Oszczędnościowe', 'check', 123.45, '2023-04-02 13:34:48.713868', null),
+    (4, '90127720550000000000000003', 'Konto na lepsze jutro', 'check', 1000.00, '2023-04-02 13:39:48.713868', null),
+    (4, '90127720550000000000000004', 'Konto dla kotka', 'check', 150.00, '2023-04-01 13:39:48.713868', null);
