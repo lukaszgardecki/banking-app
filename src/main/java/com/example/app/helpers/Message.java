1c5263af2c222bb727c424d6625cc45bf3888c22
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-04-04 19:09:17 +0200

    Create necessary messages in Message class

--- src/main/java/com/example/app/helpers/Message.java
+++ src/main/java/com/example/app/helpers/Message.java
@@ -8,17 +8,23 @@ public class Message {
     public static final String INCORRECT_AMOUNT = "Please enter the correct amount";
     public static final String TOO_LOW_BALANCE = "Too little funds in the account";
 
-    public static final String DEPOSIT_ACCOUNT_TO_EMPTY = "Account depositing to cannot be empty";
     public static final String DEPOSIT_AMOUNT_FIELD_EMPTY = "Deposit amount cannot be empty";
+    public static final String DEPOSIT_ACCOUNT_TO_EMPTY = "Account depositing to cannot be empty";
     public static final String DEPOSIT_SUCCESS = "Account deposited successfully!";
 
+    public static final String TRANSFER_AMOUNT_FIELD_EMPTY = "Transfer amount cannot be empty";
     public static final String TRANSFER_ACCOUNT_FROM_EMPTY = "The account transferring from cannot be empty";
     public static final String TRANSFER_ACCOUNT_TO_EMPTY = "The account transferring to cannot be empty";
-    public static final String TRANSFER_AMOUNT_FIELD_EMPTY = "Transfer amount cannot be empty";
     public static final String TRANSFER_ACCOUNTS_ARE_SAME = "Cannot transfer into the same account. Please select the appropriate account to perform transfer";
     public static final Object TRANSFER_SUCCESS = "Amount transferred successfully!";
 
-    public static final String WITHDRAW_ACCOUNT_FROM_EMPTY = "Account withdrawing from cannot be empty";
     public static final String WITHDRAW_AMOUNT_FIELD_EMPTY = "Withdrawal amount cannot be empty";
+    public static final String WITHDRAW_ACCOUNT_FROM_EMPTY = "Account withdrawing from cannot be empty";
     public static final String WITHDRAW_SUCCESS = "You have successfully withdraw %s from your bank account!";
+
+    public static final String PAYMENT_AMOUNT_FIELD_EMPTY = "Payment amount cannot be empty";
+    public static final String PAYMENT_BENEFICIARY_EMPTY = "Beneficiary cannot be empty";
+    public static final String PAYMENT_ACCOUNT_NO_EMPTY = "Account number cannot be empty";
+    public static final String PAYMENT_ACCOUNT_FROM_EMPTY = "Account paying from cannot be empty";
+    public static final String PAYMENT_SUCCESS = "Payment processed successfully";
 }
