bb87650d52600b42e846017476d5bfd8e752fc03
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-04-04 21:48:10 +0200

    Create liquibase database table views

--- src/main/resources/db/testdata/0004_payments.sql
+++ src/main/resources/db/testdata/0004_payments.sql
@@ -1,10 +1,10 @@
 INSERT INTO payments
-    (account_id, benerficiary, beneficiary_acc_no, amount, reference_no, status, reason_code, created_at)
+    (account_id, beneficiary, beneficiary_acc_no, amount, reference_no, status, reason_code, created_at)
 VALUES
     (4, 'Święty Mikołaj', '89123412340000000078965432', 50.00, 'Na lepszy worek', 'success', 'Payment processed successfully', '2023-04-04 18:52:15.806455'),
     (4, 'Malanowski i Partnerzy', '89123412340000000078965432', 2500.00, 'Sprawa', 'success', 'Payment processed successfully', '2023-08-02 18:52:15.806455'),
     (4, 'Kamil Kowalski', '89123412340000000078965432', 123.00, 'Ona czuje we mnie piniondz', 'success', 'Payment processed successfully', '2023-05-04 18:52:15.806455'),
-    (5, 'Ania Dobra', '89123412340000000078965432', 2742.40, 'Panie dobry jak chleb', 'success', 'Payment processed successfully', '2023-03-05 18:52:15.806455'),
+    (3, 'Ania Dobra', '89123412340000000078965432', 2742.40, 'Panie dobry jak chleb', 'success', 'Payment processed successfully', '2023-03-05 18:52:15.806455'),
     (2, 'Teresa Zwanaświętom', '89123412340000000078965432', 3009.43, 'Na złote lizaki', 'success', 'Payment processed successfully', '2023-05-06 18:52:15.806455'),
     (1, 'Mojżesz Tenodgóry', '89123412340000000078965432', 45809.27, 'Sprzedałem krzak z góry', 'success', 'Payment processed successfully', '2023-02-07 18:52:15.806455'),
     (3, 'Pani Krysia', '89123412340000000078965432', 1234.38, 'Dwa szoty poproszę', 'success', 'Payment processed successfully', '2023-01-08 18:52:15.806455');
