a9d6eff9f6319bb2bfaace7e7150bb821effcc88
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-04-07 22:57:45 +0200

    Update fields in PaymentHistory

--- src/main/java/com/example/app/transact/payment/PaymentHistory.java
+++ src/main/java/com/example/app/transact/payment/PaymentHistory.java
@@ -1,7 +1,6 @@
 package com.example.app.transact.payment;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,18 +14,14 @@ import java.time.LocalDateTime;
 public class PaymentHistory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private Long accountFromId;
-    @Size(max = 50)
+    private Long paymentId;
+    private Long accountId;
+    private Long userId;
     private String beneficiary;
-    @Size(max = 255)
     private String beneficiaryAccNo;
     private BigDecimal amount;
-    @Size(max = 255)
     private String reference;
-    @Size(max = 50)
     private String status;
-    @Size(max = 100)
     private String reasonCode;
     private LocalDateTime createdAt;
 }
