9320151ada3000fa152c7993fe5b9e87f99a3654
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-04-07 21:36:33 +0200

    Add PaymentHistory and PaymentHistoryRepository

--- src/main/java/com/example/app/transact/payment/PaymentHistory.java
+++ src/main/java/com/example/app/transact/payment/PaymentHistory.java
@@ -1,11 +1,32 @@
 package com.example.app.transact.payment;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
-//@Entity
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
 @Table(name = "v_payments")
+@Getter
+@Setter
 public class PaymentHistory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private Long accountFromId;
+    @Size(max = 50)
+    private String beneficiary;
+    @Size(max = 255)
+    private String beneficiaryAccNo;
+    private BigDecimal amount;
+    @Size(max = 255)
+    private String reference;
+    @Size(max = 50)
+    private String status;
+    @Size(max = 100)
+    private String reasonCode;
+    private LocalDateTime createdAt;
 }
