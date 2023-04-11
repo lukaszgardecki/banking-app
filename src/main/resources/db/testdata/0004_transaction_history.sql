4666aa74df35ab808e0b22b3893ee91db4fdfd36
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-04-11 19:37:29 +0200

    Fix testdata

--- src/main/resources/db/testdata/0004_transaction_history.sql
+++ src/main/resources/db/testdata/0004_transaction_history.sql
@@ -1,12 +1,12 @@
 INSERT INTO transaction_history
-    (id, account_id, transaction_type, amount, source, status, reason_code, created_at)
+    (account_id, transaction_type, amount, source, status, reason_code, created_at)
 VALUES
 
-    (1, 2, 'deposit', 10.00, 'online', 'success', 'Deposit transaction successful', '2023-01-30 13:15:11.224757'),
-    (2, 4, 'transfer', 45.00, 'online', 'success', 'Transfer transaction successful', '2023-02-11 13:15:17.998978'),
-    (3, 3, 'transfer', -45.00, 'online', 'success', 'Transfer transaction successful', '2023-02-11 13:15:17.995003'),
-    (4,	1, 'withdraw', -1.23, 'online', 'success', 'Withdrawal transaction successful', '2023-03-13 13:55:17.826398'),
-    (5,	2, 'withdraw', -5.00, 'online', 'success', 'Withdrawal transaction successful', '2023-03-29 13:55:29.154153'),
-    (6,	2, 'payment', 150.00, 'online', 'failed', 'Insufficient funds', '2023-04-09 13:57:04.881313'),
-    (7,	1, 'payment', -241.00, 'online', 'success', 'Payment transaction successful', '2023-04-09 13:57:54.914633'),
-    (8,	1, 'payment', -14.56, 'online', 'success', 'Payment transaction successful', '2023-04-09 13:58:46.638707');
+    (2, 'deposit', 10.00, 'online', 'success', 'Deposit transaction successful', '2023-01-30 13:15:11.224757'),
+    (4, 'transfer', 45.00, 'online', 'success', 'Transfer transaction successful', '2023-02-11 13:15:17.998978'),
+    (3, 'transfer', -45.00, 'online', 'success', 'Transfer transaction successful', '2023-02-11 13:15:17.995003'),
+    (1, 'withdraw', -1.23, 'online', 'success', 'Withdrawal transaction successful', '2023-03-13 13:55:17.826398'),
+    (2, 'withdraw', -5.00, 'online', 'success', 'Withdrawal transaction successful', '2023-03-29 13:55:29.154153'),
+    (2, 'payment', 150.00, 'online', 'failed', 'Insufficient funds', '2023-04-09 13:57:04.881313'),
+    (1, 'payment', -241.00, 'online', 'success', 'Payment transaction successful', '2023-04-09 13:57:54.914633'),
+    (1, 'payment', -14.56, 'online', 'success', 'Payment transaction successful', '2023-04-09 13:58:46.638707');
--- src/main/resources/db/testdata/0006_addresses.sql
+++ src/main/resources/db/testdata/0006_addresses.sql
@@ -1,10 +1,10 @@
 INSERT INTO address
-    (id, street, city, zip_code)
+    (street, city, zip_code)
 VALUES
-    (1, 'Mikołokowska 34/2', 'Katowice', '40-345'),
-    (2, 'Raciborska 3/3', 'Chorzów', '40-234'),
-    (3, 'Poniatowskiego 1a/23', 'Będzin', '40-123'),
-    (4, 'Wojewódzka 5/3', 'Bytom', '46-333'),
-    (5, 'Piękna 8/6', 'Gdańsk', '46-342'),
-    (6, 'Kwiatowa 123/4', 'Racibórz', '46-663');
+    ('Mikołokowska 34/2', 'Katowice', '40-345'),
+    ('Raciborska 3/3', 'Chorzów', '40-234'),
+    ('Poniatowskiego 1a/23', 'Będzin', '40-123'),
+    ('Wojewódzka 5/3', 'Bytom', '46-333'),
+    ('Piękna 8/6', 'Gdańsk', '46-342'),
+    ('Kwiatowa 123/4', 'Racibórz', '46-663');
 
