a6d39ed997efae3325bc9bb209e102bcdfe559f5
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-04-11 23:41:24 +0200

    Modify test data

--- src/main/resources/db/testdata/0004_transaction_history.sql
+++ src/main/resources/db/testdata/0004_transaction_history.sql
@@ -3,6 +3,13 @@ INSERT INTO transaction_history
 VALUES
 
     (2, 'deposit', 10.00, 'online', 'success', 'Deposit transaction successful', '2023-01-30 13:15:11.224757'),
+    (4, 'payment', -50.00, 'online',  'success', 'Payment processed successfully', '2023-02-01 18:52:15.806455'),
+    (4, 'payment', -2500.00, 'online',  'success', 'Payment processed successfully', '2023-02-02 18:52:15.806455'),
+    (4, 'payment', -123.00, 'online',  'success', 'Payment processed successfully', '2023-02-04 18:52:15.806455'),
+    (3, 'payment', -2742.40, 'online',  'success', 'Payment processed successfully', '2023-02-05 18:52:15.806455'),
+    (2, 'payment', -3009.43, 'online',  'success', 'Payment processed successfully', '2023-02-06 18:52:15.806455'),
+    (1, 'payment', -45809.27, 'online',  'success', 'Payment processed successfully', '2023-02-07 18:52:15.806455'),
+    (3, 'payment', -1234.38, 'online',  'success', 'Payment processed successfully', '2023-02-08 18:52:15.806455'),
     (4, 'transfer', 45.00, 'online', 'success', 'Transfer transaction successful', '2023-02-11 13:15:17.998978'),
     (3, 'transfer', -45.00, 'online', 'success', 'Transfer transaction successful', '2023-02-11 13:15:17.995003'),
     (1, 'withdraw', -1.23, 'online', 'success', 'Withdrawal transaction successful', '2023-03-13 13:55:17.826398'),
