b3240698519e0a972a2b41fbd2c2c803d98ad7c1
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-04-14 23:59:26 +0200

    Change dashboard main content design

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -5,6 +5,11 @@ const transferCard = document.querySelector(".transfer-card");
 const depositCard = document.querySelector(".deposit-card");
 const withdrawCard = document.querySelector(".withdraw-card");
 
+const fund1 = document.getElementById('fund-1');
+const fund2 = document.getElementById('fund-2');
+const fund3 = document.getElementById('fund-3');
+const fund4 = document.getElementById('fund-4');
+
 transactType.addEventListener('change', () => {
     switch (transactType.value) {
         case "":
@@ -43,3 +48,37 @@ transactType.addEventListener('change', () => {
         break;
     }
 });
+
+fund1.addEventListener('click', () => fillAndChangeMainIcons(fund1));
+fund2.addEventListener('click', () => fillAndChangeMainIcons(fund2));
+fund3.addEventListener('click', () => fillAndChangeMainIcons(fund3));
+fund4.addEventListener('click', () => fillAndChangeMainIcons(fund4));
+
+function fillAndChangeMainIcons(fund) {
+    const isNotClicked = !fund.classList.contains('filled');
+    let allFunds = document.getElementsByClassName('square-small');
+    if (isNotClicked) {
+        for (let i = 0; i < allFunds.length; i++) {
+            const el = allFunds.item(i);
+            const elIconName = fund.querySelector('a i').className;
+            const elSumDesc = document.getElementById(fund.id + '-desc').innerHTML;
+            const elSumAmount = document.getElementById(fund.id + '-amount').innerHTML;
+
+            if (el.id === fund.id) {
+                let bigSquareIcon = document.getElementById('square-big-icon');
+                let smallIcon = document.getElementById('small-icon');
+                let sumDesc = document.getElementById('sum-desc');
+                let sumAmount = document.getElementById('sum-amount');
+
+                bigSquareIcon.className = elIconName;
+                smallIcon.className = elIconName;
+                sumDesc.innerHTML = elSumDesc;
+                sumAmount.innerHTML = elSumAmount;
+
+                el.classList.add('filled');
+            } else {
+                el.classList.remove('filled');
+            }
+        }
+    }
+}
